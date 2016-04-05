95306d2b58c3362a90436bfc4c1be2630b99b60e
Dipro <deep.row4@gmail.com>
2016-04-05 08:43:34 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Poker App!"
       redirect_to @user
     else
       render 'new'
@@ -25,6 +25,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
-  
 end
